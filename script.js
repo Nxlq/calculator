bfc735a4b507d6b8864c060798cf96c96e49b370
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-01-19 14:21:39 -0500

    add data attributes/ ability to display current operand
    create functions to append current operand and update that current
    operand to the display, script.js now uses data attributes to select the
    desired buttons

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 "use strict";
+const outputDiv = document.querySelector(".output-div");
+const numberBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const clearBtn = document.querySelector("[data-clear]");
+const equalsBtn = document.querySelector("[data-equals]");
+const currentOperandDisplay = document.querySelector(".current-input");
+const pastOperandDisplay = document.querySelector(".past-input");
+
+let currentOperand = "";
+let pastOperand = "";
+let number = "";
+
+console.log({ numberBtns });
+console.log({ operatorBtns });
+console.log({ clearBtn });
+console.log({ equalsBtn });
+console.log({ currentOperandDisplay }, { pastOperandDisplay });
+
 function add(a, b) {
   return a + b;
 }
@@ -21,4 +39,17 @@ function calculate(a, b, operator) {
   return result;
 }
 
-calculate(7, 2, add);
+function appendOperand(numberStr) {
+  currentOperand += numberStr;
+}
+
+function updateDisplay(currentOperand) {
+  currentOperandDisplay.innerText = currentOperand;
+}
+
+numberBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    appendOperand(btn.innerText);
+    updateDisplay(currentOperand);
+  })
+);
